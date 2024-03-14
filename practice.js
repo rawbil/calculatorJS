31f75bd525392432b82d16bcdbb736cae73467cc
Bil <bildadsimiyu6@gmail.com>
2024-03-14 18:03:23 +0300

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,30 @@
+let inputBox = document.getElementById("inputBox");
+let buttons = document.querySelectorAll("button");
+
+let string = "";
+
+buttons.forEach(button => {
+   button.addEventListener('click', (e) => {
+      if(e.target.innerHTML === "=") {
+         string = eval(string);
+         inputBox.value = string;
+      }
+
+      else if(e.target.innerHTML === "AC") {
+         string = "";
+         inputBox.value = string;
+      }
+
+      else if(e.target.innerHTML === "DEL") {
+         string = string.slice(0, string.length - 1);
+         inputBox.value = string;
+      }
+
+      else {
+        string += e.target.innerHTML;
+         inputBox.value = string;
+      }
+   
+      
+   })
+})
